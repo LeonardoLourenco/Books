44a6b5966936e5f24ce18367cfd328fdefb1f7b7
Leonardo Lourenço <leonardo-lourenco@hotmail.com>
2018-05-30 17:19:36 +0100

    Creation of class DbTableBooks

--- app/src/main/java/pt/ipg/books/DbTableCategorias.java
+++ app/src/main/java/pt/ipg/books/DbTableCategorias.java
@@ -6,7 +6,7 @@ import android.provider.BaseColumns;
 public class DbTableCategorias implements BaseColumns {
     public static final String TABLE_NAME = "categories";
 
-    public static final String FIELD_NAME = "name";
+    private static final String FIELD_NAME = "name";
     private SQLiteDatabase db;
 
     public DbTableCategorias(SQLiteDatabase db) {
